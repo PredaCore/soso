49e75d8552f7d5c12d18e7e216f990822c40a153
PredaCore <34812450+PredaCore@users.noreply.github.com>
2017-12-28 12:21:20 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,10 +2,15 @@ const Discord = require("discord.js");
                         
 var bot = new Discord.Client();
 
+var promoteGrade = function(a,b,c,d) {
+    client.guilds.members.find('id', a).removeRole("id", b);
+    client.guilds.members.find('id', a).addRole("id", c);
+    if(d) client.guilds.members.find("id", a).setVoiceChannel(d);
+};
+
 bot.on("ready", function() {
-    bot.user.setGame("Race : Orc");
+    bot.user.setGame("Orc");
     console.log("le bot a bien ete connecte");
 });
-
 
 bot.login("Mzk0NTQ4NjQ2MTI3MDc1MzI5.DSU33g.4pNyKqbzGPoNSs-joO8FxxGNI7A");
